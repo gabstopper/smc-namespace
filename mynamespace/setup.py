2eb7a280b7437f18d152349939c48faa9eeda213
David LePage <dwlepage70@gmail.com>
2018-02-04 11:51:00 -0600

    3

---
+++ mynamespace/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mysmc-python',
+    packages=find_packages(),
+    zip_safe=False,
+)
---
+++ mynamespace/subpackage_a/mobuleb.py
@@ -0,0 +1,10 @@
+'''
+Created on Feb 4, 2018
+
+@author: davidlepage
+'''
+
+from mynamespace.subpackage_a import modulea
+
+if __name__ == '__main__':
+    print(modulea.name)
